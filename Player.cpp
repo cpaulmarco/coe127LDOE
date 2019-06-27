3c68a983e979217bdbaea048bb887aef90087774
peeejay04 <patrickjmagno@gmail.com>
2019-06-27 20:13:08 +0800

    Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Player::~Player()
+    {
+
+    }
+    void Player::setName(std::string n)
+    {
+        name = n;
+    }
+    void Player::setScore(int s)
+    {
+    return name;
+    }
+    int Player::getScore()
+    {
+    return score;
+    }
+    return 0;
+}
